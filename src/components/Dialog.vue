fcf833505f593d2a55470cde09142e42d1ddacee
shotaro-pd <shotakosan.script@gmail.com>
2021-01-18 22:08:03 +0900

    focus設定追加

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -15,7 +15,7 @@
           <v-container>
             <v-row>
               <v-col cols="12">
-                <v-text-field label="タスク名" v-model="taskName" required ref="task"></v-text-field>
+                <v-text-field label="タスク名" v-model="taskName" required autofocus></v-text-field>
               </v-col>
               <v-col cols="6">
                 <v-text-field label="タグ名" v-model="tagName"></v-text-field>
@@ -49,9 +49,6 @@
         this.taskName = null
         this.tagName = null
       }
-    },
-    mounted: function() {
-      this.$refs.task.focus();
     }
   }
 </script>
